070d9acc006df190bcf67130fedba40f5e8599bb
Faiza RABHI <93930498+Faizarabhi@users.noreply.github.com>
2024-01-30 02:10:16 +0100

    feat add function _printf

---
+++ _printf.c
@@ -0,0 +1,34 @@
+int _printf(const char *format, ...)
+{
+    va_list ptr;
+    int i;
+    char f;
+    char *s;
+
+    va_start(ptr, format);
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == 's')
+            {
+                s = va_arg(ptr, char *);
+               write(1, s, _strlen(s));
+            }
+            else if (format[i + 1] == 'c')
+            {
+                f = va_arg(ptr, int);
+                write(1, &f, 1);
+            }
+            else if (format[i + 1] == '%')
+            {
+                write(1, &format[i], 1);
+            }
+            i++;
+        }
+        else
+            printf("%c", format[i]);
+    }
+
+    va_end(ptr);
+}
